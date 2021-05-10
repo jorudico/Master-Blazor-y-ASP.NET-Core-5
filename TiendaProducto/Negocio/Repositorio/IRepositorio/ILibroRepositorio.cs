383bd9598492b5866dd3363ccd0a85c0dc369082
jorudico <jorudico@gmail.com>
2021-05-10 08:34:47 -0400

    44 creacion de interface ILibtoRepositorio

---
+++ TiendaProducto/Negocio/Repositorio/IRepositorio/ILibroRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Repositorio.IRepositorio
+{
+  public interface ILibroRepositorio
+    {
+
+        public Task<LibroDTO> AgregarLibro(LibroDTO libroDTO);
+        public Task<LibroDTO> ActualizarLibro(int LibroId,LibroDTO libroDTO);
+        public Task<LibroDTO> ObtenerLibro(int LibroId);
+        public Task<IAsyncEnumerable<LibroDTO>> ObtenerListaLibro();
+        public Task<LibroDTO> ExisteNombreLibro(string Nombre);
+
+
+
+    }
+}
