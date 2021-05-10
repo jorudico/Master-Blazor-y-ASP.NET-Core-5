60add8f98d1cb2a90f9fedb3d9ae1e5a6a795dcf
jorudico <jorudico@gmail.com>
2021-05-10 08:28:55 -0400

    43 Creacion de LibroDTO

---
+++ TiendaProducto/Modelo/LibroDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+   public class LibroDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Por favor ingresa Nombre de libro")]
+        public string Nombre { get; set; }
+        [Range(10,100,ErrorMessage ="El precio regular debe estar entre 10 y 100")]
+        public double PrecioRegular { get; set; }
+        [Required(ErrorMessage = "Por favor ingresa Nombre de Autor")]
+        public string Autor { get; set; }
+        public string Detalles { get; set; }
+    }
+}
